9ba64acb70129545f9fb28e935917dbf24af5b34
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-03 15:00:13 -0300

    feat: add code of enums

---
+++ fundamentals/04-arrays-enums.ts
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3];
+
+// Função para adicionar um número no final do array
+numbers.push(4);
+
+// como mostrar um array de numeros no console.log?
---
+++ fundamentals/05-enums.ts
@@ -0,0 +1,13 @@
+enum CourseType {
+    FREE = "FREE",
+    PREMIUM = "PREMIUM",
+    PRIVATE = "PRIVATE",
+    HIDDEN = "HIDDEN"
+}
+
+const course = {
+    title: "Typescript Bootcamp",
+    type: CourseType.HIDDEN
+};
+
+console.log(course);
