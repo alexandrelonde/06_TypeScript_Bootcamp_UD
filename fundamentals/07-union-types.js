a44f42cbf66324fea06a3dfea51be363f6bc7eaf
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-04 09:27:42 -0300

    feat: add union types

--- fundamentals/07-union-types.js
+++ fundamentals/07-union-types.js
@@ -1,22 +1,13 @@
-
 console.log("\n");
-
 // UNION TYPES
 // ===========
-
-let uniqueIdentifier: number | string = 1000;
-
+var uniqueIdentifier = 1000;
 uniqueIdentifier = "201e72bb-49e3-40ef-8331-b7f3e7d947f8";
-
 // Para utilizar union types em um array de number ou strings
-
-const keys: (number | string) [] = [1000, "Hello"];
-
+var keys = [1000, "Hello"];
 // Boa prática de tipar uma variável que não é obrigatória mas 
 // precisa de um tipo específico ao ser preenchida
-let courseId: number | null = 1000;
-
+var courseId = 1000;
 courseId = null;
-
 console.log("UNION TYPES");
 console.log(courseId);
