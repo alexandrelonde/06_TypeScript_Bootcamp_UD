92cb36c632fad6a0b8ad79f419a6c4966309103a
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-04 09:09:12 -0300

    feat: add code of union types

---
+++ fundamentals/07-union-types.ts
@@ -0,0 +1,15 @@
+
+console.log("\n");
+
+// UNION TYPES
+// ===========
+
+let uniqueIdentifier: number | string = 1000;
+
+uniqueIdentifier = "201e72bb-49e3-40ef-8331-b7f3e7d947f8";
+
+// Para utilizar union types em um array de number ou strings
+
+const keys: (number | string) [] = [1000, "Hello"];
+
+
