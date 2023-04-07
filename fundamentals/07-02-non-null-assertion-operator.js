295dbf7a20808e863a5ea8deb384eefc558b3a6e
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-04-07 08:12:16 -0300

    feat: add non null assertion operator

---
+++ fundamentals/07-02-non-null-assertion-operator.js
@@ -0,0 +1,7 @@
+var courseId;
+/*
+    ts(2451) significa que estou usando uma variável com o
+    mesmo nome em outro arquivo do projeto e o typescript
+    está me mostrando isso, caso eu queira utilizar um import.
+*/
+courseId === null || courseId === void 0 ? void 0 : courseId.toString();
---
+++ fundamentals/07-02-non-null-assertion-operator.ts
@@ -0,0 +1,10 @@
+let courseId : number | null;
+
+/*
+    ts(2451) significa que estou usando uma variável com o 
+    mesmo nome em outro arquivo do projeto e o typescript
+    está me mostrando isso, caso eu queira utilizar um import.
+*/
+
+courseId?.toString();
+
